ec76bfc41fc877d85e3bc1e5f98dc4ca64838f9f
Paul Furgale <paul.furgale@gmail.com>
2013-04-28 19:34:51 +0200

    Even simpler. A's implementation moved to the header

--- src/A.cpp
+++ src/A.cpp
@@ -12,18 +12,5 @@
 A::A(){}
 A::~A(){}
 
-BOOST_CLASS_EXPORT_IMPLEMENT( A );
 
 
-template<class Archive>
-void A::load(Archive & ar, const unsigned int version){}
-    
-template<class Archive>
-void A::save(Archive & ar, const unsigned int version) const{}
-
-template void A::save(boost::archive::binary_oarchive & ar, const unsigned int version) const;
-template void A::load(boost::archive::binary_iarchive & ar, const unsigned int version);
-template void A::save(boost::archive::text_oarchive & ar, const unsigned int version) const;
-template void A::load(boost::archive::text_iarchive & ar, const unsigned int version);
-template void A::save(boost::archive::xml_oarchive & ar, const unsigned int version) const;
-template void A::load(boost::archive::xml_iarchive & ar, const unsigned int version);
